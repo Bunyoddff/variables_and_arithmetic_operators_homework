654bf871f06a98ab60e02e5c9fc729606f973343
Bunyoddff <bkhusanov444@gmail.com>
2022-12-20 14:13:05 +0500

    py09

--- arithmetic_op09.py
+++ arithmetic_op09.py
@@ -5,15 +5,6 @@
 # Create a variable "c"  and assign it the value of the int 2.
 
 # Create a variable "d"  and assign it the value of the int 4.
-
-# Create a variable called 'answer' and assign it the expression value: a+b - cd\
-# a=8
-# b=3
-# c=2
-# d=4
-# answer=a+b-c*d
-# print(answer)
-
 x=5
 x*=2
 print(x)
