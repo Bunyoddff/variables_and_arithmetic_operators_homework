516eec5e244c161cc70c33fd8a9f963c4a8b108b
Bunyoddff <bkhusanov444@gmail.com>
2022-12-20 11:17:40 +0500

    easy2

--- arithmetic_op01.py
+++ arithmetic_op01.py
@@ -3,3 +3,7 @@
 # Create a variable 'num_two'   and assign it the value of the int 4.
 
 # Create a variable 'variable_total' and assign it the sum of  'num_one' and 'num_two'.
+num_two =5
+num_one =4
+variable_total=num_two+num_one
+print(variable_total)
